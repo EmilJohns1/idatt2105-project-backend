226a8b0003d158d6259e1461c97f1822451ad9a7
1Cezzo <emiljohnzen@gmail.com>
2024-03-21 13:22:15 +0100

    initial commit

---
+++ src/test/java/com/IDATT2105/IDATT2105Backend/Idatt2105BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IDATT2105.IDATT2105Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Idatt2105BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
