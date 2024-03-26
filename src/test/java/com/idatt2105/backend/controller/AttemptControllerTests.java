5b2c3e1764a04fbad22ed79256fe5a8cf472e9a9
Svein Kåre <sveiks@stud.ntnu.no>
2024-03-26 14:27:46 +0100

    Fixed controller tests that were broken by Spring Security

--- src/test/java/com/idatt2105/backend/controller/AttemptControllerTests.java
+++ src/test/java/com/idatt2105/backend/controller/AttemptControllerTests.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,10 +21,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(AttemptController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class AttemptControllerTests {
   @Autowired MockMvc mockMvc;
-
   @MockBean private AttemptService attemptService;
 
   @Test
