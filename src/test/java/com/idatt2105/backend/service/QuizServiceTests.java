370ffc348332d1ef6da536b52d07c89fe347a73b
Svein Kåre <sveiks@stud.ntnu.no>
2024-03-27 15:02:26 +0100

    Refactored some methods in QuizService, and added more unit tests

--- src/test/java/com/idatt2105/backend/service/QuizServiceTests.java
+++ src/test/java/com/idatt2105/backend/service/QuizServiceTests.java
@@ -1,5 +1,11 @@
 package com.idatt2105.backend.service;
 
+import com.idatt2105.backend.dto.UserDTO;
+import com.idatt2105.backend.model.User;
+import com.idatt2105.backend.repository.UserRepository;
+import com.idatt2105.backend.util.InvalidIdException;
+import com.idatt2105.backend.util.UserNotFoundException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -33,6 +39,7 @@ public class QuizServiceTests {
 
   @Mock private TagRepository tagRepository;
   @Mock private QuizRepository quizRepository;
+  @Mock private UserRepository userRepository;
 
   @BeforeEach
   void setUp() {
@@ -44,6 +51,100 @@ public class QuizServiceTests {
 
   @Nested
   class BasicFunctionality {
+    @Test
+    void getAllQuizzes() {
+      Quiz quiz = new Quiz();
+      quiz.setId(1L);
+      quiz.setTitle("Quiz");
+      when(quizRepository.findAll()).thenReturn(List.of(quiz));
+      List<QuizDTO> actual = quizService.getAllQuizzes();
+      assertEquals(new QuizDTO(quiz), actual.get(0));
+    }
+
+    @Test
+    void getQuizById() {
+      Quiz quiz = new Quiz();
+      quiz.setId(1L);
+      quiz.setTitle("Quiz");
+      when(quizRepository.findById(1L)).thenReturn(Optional.of(quiz));
+      QuizDTO actual = quizService.getQuizById(1L);
+      assertEquals(new QuizDTO(quiz), actual);
+    }
+
+    @Test
+    void saveStoresEntityInRepository() {
+      Quiz input = new Quiz();
+      input.setId(1L);
+      input.setTitle("Quiz");
+      QuizDTO actual = quizService.save(input);
+      assertEquals(new QuizDTO(input), actual);
+      verify(quizRepository).save(input);
+    }
+
+    @Test
+    void deleteQuizCallsDeleteByIdOnRepository() {
+      quizService.deleteQuiz(1L);
+      verify(quizRepository).deleteById(1L);
+    }
+
+    @Test
+    void updateQuizCallsRepository() {
+      QuizDTO dto = new QuizDTO();
+      dto.setId(1L);
+      dto.setTitle("New Title");
+      dto.setDescription("New Description");
+      quizService.updateQuiz(1L, dto);
+      verify(quizRepository).save(any(Quiz.class));
+    }
+
+    @Test
+    void addUserToQuizCallsSaveOnQuizRepository() {
+      Quiz quiz = new Quiz();
+      User user = new User();
+      user.setQuizzes(new HashSet<>());
+      quiz.setUsers(new HashSet<>());
+      when(quizRepository.findById(1L)).thenReturn(Optional.of(quiz));
+      when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+      quizService.addUserToQuiz(1L, 1L);
+      verify(quizRepository).save(quiz);
+    }
+
+    @Test
+    void deleteUserFromQuizCallsSaveOnQuizRepository() {
+      Quiz quiz = new Quiz();
+      User user = new User();
+      user.setQuizzes(new HashSet<>());
+      quiz.setUsers(new HashSet<>());
+      when(quizRepository.findById(1L)).thenReturn(Optional.of(quiz));
+      when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+      quizService.addUserToQuiz(1L, 1L);
+      verify(quizRepository).save(quiz);
+    }
+
+    @Test
+    void getQuizByTitle() {
+      Quiz quiz = new Quiz();
+      quiz.setId(1L);
+      quiz.setTitle("Quiz");
+      when(quizRepository.findByTitle("Quiz")).thenReturn(Optional.of(quiz));
+      QuizDTO actual = quizService.getQuizByTitle("Quiz");
+      assertEquals(new QuizDTO(quiz), actual);
+    }
+
+    @Test
+    void getUsersByQuizId() {
+      User user = new User();
+      user.setId(1L);
+      user.setUsername("User");
+      Quiz quiz = new Quiz();
+      quiz.setId(1L);
+      quiz.setUsers(new HashSet<>(List.of(user)));
+      when(quizRepository.findById(1L)).thenReturn(Optional.of(quiz));
+
+      Set<UserDTO> actual = quizService.getUsersByQuizId(1L);
+      assertEquals(new UserDTO(user), actual.iterator().next());
+    }
+
     @Test
     void addTagsAddsNonExistingTagsToTagRepository() {
       QuizDTO input = new QuizDTO();
@@ -110,6 +211,75 @@ public class QuizServiceTests {
 
   @Nested
   class InvalidParameters {
+    @Test
+    void getQuizByIdThrowsExceptionWhenGivenInvalidId() {
+      assertThrows(InvalidIdException.class, () -> quizService.getQuizById(2L));
+    }
+
+    @Test
+    void getQuizByIdThrowsExceptionWhenGivenNull() {
+      assertThrows(IllegalArgumentException.class, () -> quizService.getQuizById(null));
+    }
+
+    @Test
+    void saveThrowsExceptionWhenGivenNullAsParameter() {
+      assertThrows(IllegalArgumentException.class, () -> quizService.save(null));
+    }
+
+    @Test
+    void deleteQuizThrowsExceptionWhenGivenNullAsParameter() {
+      assertThrows(IllegalArgumentException.class, () -> quizService.deleteQuiz(null));
+    }
+
+    @Test
+    void updateQuizThrowsExceptionWhenGivenNullAsIdParameter() {
+      QuizDTO dto = new QuizDTO();
+      assertThrows(IllegalArgumentException.class, () -> quizService.updateQuiz(null, dto));
+    }
+
+    @Test
+    void updateQuizThrowsExceptionWhenGivenNullAsDtoParameter() {
+      assertThrows(IllegalArgumentException.class, () -> quizService.updateQuiz(1L, null));
+    }
+
+    @Test
+    void addUserToQuizThrowsExceptionWhenUserIdIsNull() {
+      assertThrows(IllegalArgumentException.class, () -> quizService.addUserToQuiz(null, 1L));
+    }
+
+    @Test
+    void addUserToQuizThrowsExceptionWhenQuizIdIsNull() {
+      assertThrows(IllegalArgumentException.class, () -> quizService.addUserToQuiz(1L, null));
+    }
+
+    @Test
+    void addUserToQuizThrowsExceptionWhenUserIsNotFound() {
+      Quiz quiz = new Quiz();
+      quiz.setUsers(new HashSet<>());
+      when(quizRepository.findById(2L)).thenReturn(Optional.of(quiz));
+      assertThrows(InvalidIdException.class, () -> quizService.addUserToQuiz(1L, 1L));
+    }
+
+    @Test
+    void deleteUserFromQuizThrowsExceptionWhenUserIdIsNull() {
+      assertThrows(IllegalArgumentException.class, () -> quizService.removeUserFromQuiz(null, 1L));
+    }
+
+    @Test
+    void deleteUserFromQuizThrowsExceptionWhenQuizIdIsNull() {
+      assertThrows(IllegalArgumentException.class, () -> quizService.removeUserFromQuiz(1L, null));
+    }
+
+    @Test
+    void getQuizByTitleThrowsExceptionWhenGivenNullAsParameter() {
+      assertThrows(IllegalArgumentException.class, () -> quizService.getQuizByTitle(null));
+    }
+
+    @Test
+    void getUsersByQuizIdThrowsExceptionWhenGivenNullAsParameter() {
+      assertThrows(IllegalArgumentException.class, () -> quizService.getUsersByQuizId(null));
+    }
+
     @Test
     void addTagsThrowsExceptionWhenGivenNullAsParameter() {
       assertThrows(IllegalArgumentException.class, () -> quizService.addTags(null));
