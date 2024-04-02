8750a60cfc4aa1518eba87cf1147edc3adb0a6c7
SveinKare <sksorestad@gmail.com>
2024-04-02 18:50:54 +0200

    Increased access token duration to 3 hours

--- src/main/java/com/idatt2105/backend/security/AuthorizationServerConfig.java
+++ src/main/java/com/idatt2105/backend/security/AuthorizationServerConfig.java
@@ -26,7 +26,6 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
-import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -82,8 +81,7 @@ public class AuthorizationServerConfig {
                     .build())
             .tokenSettings(
                 TokenSettings.builder()
-                    .refreshTokenTimeToLive(Duration.of(24, ChronoUnit.HOURS))
-                    .accessTokenTimeToLive(Duration.of(30, ChronoUnit.MINUTES))
+                    .accessTokenTimeToLive(Duration.of(180, ChronoUnit.MINUTES))
                     .build())
             .build();
 
