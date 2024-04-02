cc2ce439a1e8c92de581353794918338bde0187b
1Cezzo <emiljohnzen@gmail.com>
2024-04-03 00:39:50 +0200

    added update alternatives endpoint

--- src/main/java/com/idatt2105/backend/controller/QuestionController.java
+++ src/main/java/com/idatt2105/backend/controller/QuestionController.java
@@ -5,14 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.idatt2105.backend.dto.AlternativeDTO;
 import com.idatt2105.backend.dto.QuestionDTO;
@@ -136,6 +129,14 @@ public class QuestionController {
   public ResponseEntity<Void> deleteAlternative(@PathVariable Long id) {
     questionService.deleteAlternative(id);
     return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @PutMapping("/{questionId}/alternatives")
+  public ResponseEntity<List<Alternative>> updateAlternatives(
+    @PathVariable Long questionId,
+    @RequestBody List<AlternativeDTO> alternativeDTOs) {
+    List<Alternative> updatedAlternatives = questionService.updateAlternatives(questionId, alternativeDTOs);
+    return new ResponseEntity<>(updatedAlternatives, HttpStatus.OK);
   }
 
   /**
