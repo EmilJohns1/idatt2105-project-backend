5fcf5347d5caad0a6ab127713d3f6ad2a09c09f3
SveinKare <sksorestad@gmail.com>
2024-04-06 23:24:16 +0200

    Added more test data

--- src/main/java/com/idatt2105/backend/util/DataLoader.java
+++ src/main/java/com/idatt2105/backend/util/DataLoader.java
@@ -1,5 +1,6 @@
 package com.idatt2105.backend.util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,11 +10,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import com.idatt2105.backend.dto.AlternativeDTO;
+import com.idatt2105.backend.dto.CommentDTO;
 import com.idatt2105.backend.dto.QuestionDTO;
 import com.idatt2105.backend.dto.QuizDTO;
 import com.idatt2105.backend.enumerator.QuestionType;
 import com.idatt2105.backend.model.Category;
 import com.idatt2105.backend.model.User;
+import com.idatt2105.backend.service.CommentService;
 import com.idatt2105.backend.service.QuestionService;
 import com.idatt2105.backend.service.QuizService;
 import com.idatt2105.backend.service.UserService;
@@ -29,12 +32,17 @@ public class DataLoader implements ApplicationListener<ApplicationReadyEvent> {
   private final UserService userService;
   private final QuizService quizService;
   private final QuestionService questionService;
+  private final CommentService commentService;
 
   public DataLoader(
-      UserService userService, QuizService quizService, QuestionService questionService) {
+      UserService userService,
+      QuizService quizService,
+      QuestionService questionService,
+      CommentService commentService) {
     this.userService = userService;
     this.quizService = quizService;
     this.questionService = questionService;
+    this.commentService = commentService;
   }
 
   @Override
@@ -70,11 +78,31 @@ public class DataLoader implements ApplicationListener<ApplicationReadyEvent> {
           }
         });
     if (quizService.getAllQuizzes(Pageable.ofSize(1)).isEmpty()) { // No quizzes are stored
+      // Make 3 users to show off comments
+      List<Long> ids = new ArrayList<>();
+      User user = new User();
+      user.setUsername("John");
+      user.setPassword("password");
+      ids.add(userService.addUser(user).getId());
+
+      User user2 = new User();
+      user2.setUsername("Jane");
+      user2.setPassword("password");
+      ids.add(userService.addUser(user2).getId());
+
+      User user3 = new User();
+      user3.setUsername("Alice");
+      user3.setPassword("password");
+      ids.add(userService.addUser(user3).getId());
+
+      String[] comments =
+          new String[] {"This quiz is great!", "I love this quiz!", "This quiz is so fun!"};
+
       for (String category : categoryNames) {
         for (int i = 0; i < 5; i++) { // Create 5 quizzes for each category
           QuizDTO quiz =
               new QuizDTO.Builder()
-                  .setTitle("Quiz title")
+                  .setTitle("The Fun Quiz")
                   .setDescription(
                       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
                           + "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, "
@@ -85,10 +113,19 @@ public class DataLoader implements ApplicationListener<ApplicationReadyEvent> {
                   .setRandomizedOrder(false)
                   .build();
           Long savedId = quizService.save(quiz).getId();
+
+          for (int j = 0; j < 3; j++) {
+            CommentDTO comment = new CommentDTO();
+            comment.setContent(comments[j]);
+            comment.setQuizId(savedId);
+            comment.setUserId(ids.get(j));
+            commentService.saveComment(comment);
+          }
+
           quizService.addUserToQuiz(1L, savedId);
           Long questionId;
           QuestionDTO question = new QuestionDTO();
-          question.setPoints(360);
+          question.setPoints(5);
           question.setQuizId(savedId);
 
           question.setType(QuestionType.TRUE_OR_FALSE);
@@ -102,6 +139,18 @@ public class DataLoader implements ApplicationListener<ApplicationReadyEvent> {
           question.setQuestionText(
               "The Great Wall of China is visible from space with the naked eye.");
           question.setIsCorrect(false);
+          questionId = questionService.addQuestion(question).getId();
+          question.setQuestionId(questionId);
+          questionService.updateTrueOrFalseQuestion(question);
+
+          question.setQuestionText("Sharks are mammals.");
+          question.setIsCorrect(false);
+          questionId = questionService.addQuestion(question).getId();
+          question.setQuestionId(questionId);
+          questionService.updateTrueOrFalseQuestion(question);
+
+          question.setQuestionText("Octopuses have three hearts.");
+          question.setIsCorrect(true);
           questionId = questionService.addQuestion(question).getId();
           question.setQuestionId(questionId);
           questionService.updateTrueOrFalseQuestion(question);
